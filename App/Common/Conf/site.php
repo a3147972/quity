0e78df637351ca7c85180c4be69ea474b3ff55dc
a3147972 <2387813033@qq.com>
2015-11-23 15:19:05 +0800

    1

--- App/Common/Conf/site.php
+++ App/Common/Conf/site.php
@@ -3,6 +3,7 @@ return array (
   'open_transfer_status' => '1',
   'open_status' => '1',
   'open_time' => '00:00:00 - 23:59:59',
-  'lock_time' => '',
+  'lock_time' => '1|2|3',
+  'deal_time' => '1|2|3',
   'share_amount' => '1',
 );
